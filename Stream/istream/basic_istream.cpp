df97437ccce045e3a9bdd862b957d5878ed12a79
AkshayaBaalajiS <baalaji12akshay@gmail.com>
2025-08-27 10:19:56 +0530

    Added new concepts

---
+++ Stream/istream/basic_istream.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"istream in c++\n";
+	int x ;
+	cin >> x; // this cin is a global obj of istream in C++;
+
+	// some function from istream which will be useful 
+}
