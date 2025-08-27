b0b65670ec25cd575df8320eeb02be888fc5521c
AkshayaBaalajiS <baalaji12akshay@gmail.com>
2025-08-27 12:02:41 +0530

    Stream files added

--- Stream/istream/basic_istream.cpp
+++ Stream/istream/basic_istream.cpp
@@ -7,5 +7,54 @@ int main()
 	int x ;
 	cin >> x; // this cin is a global obj of istream in C++;
 
+	/*
+	cin >> x; reads the integer 10.
+
+	But when you hit Enter, a newline character '\n' is left behind in the input buffer.
+
+	So the buffer looks like this after cin >> x;:
+
+	so we use ignore 
+	*/
+	cin.ignore(); // for discarding the new line from the input 
+
 	// some function from istream which will be useful 
+	char c;
+	cin.get(c);
+
+	cout<<"Int  = " << x <<endl;
+	cout<<"Char = " << c <<endl;
+
+	cin.ignore(1000, '\n');
+	/*
+	Step 1: After cin >> num
+	User input:
+	123⏎
+	cin >> num reads the number 123.
+	Leftover in the buffer:
+	\n
+
+	Step 2: After cin.ignore(1000, '\n')
+	ignore(1000, '\n') starts reading the buffer character by character.
+	It sees '\n' immediately → stops ignoring.
+	Buffer now:
+	(empty)
+
+	Only 1 character was ignored, not 1000. ✅
+	Step 3: cin.get(c) reads next input
+	Buffer is empty, so cin.get(c) waits for the user to type a character.
+	Suppose user types A + Enter:
+
+	*/
+
+	string strr;
+	cin>>strr;
+
+
+	cout<<strr<<endl;
+
+	cin.ignore(10000, '\n'); // when we are giving the string with space in string input with cin the string befoe space will be taked and rem will be passed to next input so for clearing buffer we are giving it
+
+	getline(cin,strr);
+	cout<<strr<<endl;
 }
---
+++ Stream/ostream/basic_ostream.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"This is the simple print stmt \n";
+
+	cout.put('a');
+
+	int a =2134;
+
+	cout<<"\nPrint through the cout \n";
+	cout.put('a');	
+
+	cout.write("\nThis is the simple print stmt ", 32); 
+}
