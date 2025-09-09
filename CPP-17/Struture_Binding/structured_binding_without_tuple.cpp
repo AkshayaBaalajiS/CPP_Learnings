6e985dd6d6714f8b7cab20e5a1b36a5ddcc86f02
AkshayaBaalajiS <baalaji12akshay@gmail.com>
2025-09-09 22:05:52 +0530

    Structure binding

---
+++ CPP-17/Struture_Binding/structured_binding_without_tuple.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <tuple>
+using namespace std;
+
+struct A 
+{
+	public:
+		A(int a, float b, char c, double d, string e) : a(a),b(b), c(c), d(d), e(e)
+		{
+			cout<<"@ A class constructor \n";
+		}
+
+		int a;
+		float b;
+		char c;
+		double d;
+		string e;
+
+		// we are commenting the tuple return and doing structure binding without tuple 
+		// tuple <int , float, char , double, string> operator()()
+		// {
+		// 	cout<<"@ tuple <int , float, char , double, string> operator()() \n";
+		// 	return make_tuple(a,b,c,d,e);
+		// }
+
+		~A()
+		{
+			cout<<"@ A class destructor \n";
+		}
+};
+
+int main()
+{
+	cout<<"This is the program to understand the structure binding without the tuple\n";
+
+	A object(12,12.121f,'a',32232.2233232,"Akshaya Baalaji Senthilraj");
+
+	auto[a,b,c,d,e] = object;
+
+	cout<<"A = " << a <<endl;
+	cout<<"B = " << b <<endl;
+	cout<<"C = " << c <<endl;
+	cout<<"D = " << d <<endl;
+	cout<<"E = " << e <<endl;
+}
