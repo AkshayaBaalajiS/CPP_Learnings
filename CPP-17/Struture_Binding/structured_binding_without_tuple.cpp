2e1fd5fafefbb602e459ea0cb96963b32c5536a7
AkshayaBaalajiS <baalaji12akshay@gmail.com>
2025-09-13 14:47:16 +0530

    updation on sturctured binding

--- CPP-17/Struture_Binding/structured_binding_without_tuple.cpp
+++ CPP-17/Struture_Binding/structured_binding_without_tuple.cpp
@@ -35,7 +35,13 @@ int main()
 
 	A object(12,12.121f,'a',32232.2233232,"Akshaya Baalaji Senthilraj");
 
-	auto[a,b,c,d,e] = object;
+	// auto[a,b,c,d,e] = object;
+	// int [a,b,c,d,e] = object; // this will not work cause of below reason 
+	/*
+Why only auto?
+
+Because the compiler needs to deduce multiple types at once. Each element can have a different type (e.g. int, double, std::string). C++ has no syntax to express "multiple variable types" in a single declaration other than auto.
+	*/
 
 	cout<<"A = " << a <<endl;
 	cout<<"B = " << b <<endl;
