11a757f517b9ce602b1982fe2c7d99d67e70a31f
AkshayaBaalajiS <baalaji12akshay@gmail.com>
2025-11-17 09:10:58 +0530

    Delete for simple function

---
+++ Special_Member_Functions/delete/delete_understanding.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+// unlike the default delete can be applied for any function 
+int program(int a) = delete; 
+
+int main()
+{
+	cout<<"Simple progarm to understand the delete in cpp \n";
+
+}
