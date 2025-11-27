7ccb0d1363f26dfa325d3676bcfbd6b1bfa1f326
AkshayaBaalajiS <baalaji12akshay@gmail.com>
2025-11-27 10:33:32 +0530

    STL with CTAD udnerstanding and the map cant be CTAD

---
+++ CPP-17/ClsTempArgDeduc/ctad_stl.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <map>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	vector vobj={12,23,34,45};
+	// vector vobj1={12,23,34,4,12.125}; // this cant be done cause should have all values of same type 
+
+	// map mapObj = {{1,23}}; this cant be done 
+	map<int, int> mapObj = {{1,23}}; 
+	
+
+	pair pairObj = {1,23}; 
+
+}
