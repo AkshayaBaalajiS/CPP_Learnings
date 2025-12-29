34a6c029943324e24bc1851086a50871e58ce9ea
AkshayaBaalajiS <baalaji12akshay@gmail.com>
2025-12-29 09:54:31 +0530

    Structure padding and alignmentin cpp

---
+++ Structure/Structure_Padding/structure_padding_2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+struct A 
+{
+	int a ;
+	char aa;
+	char aa1;
+	char aa2;
+	char aa3;
+	char aa4;
+	
+};
+
+int main()
+{
+	cout<<"Size of a  = " <<sizeof(A);
+}
