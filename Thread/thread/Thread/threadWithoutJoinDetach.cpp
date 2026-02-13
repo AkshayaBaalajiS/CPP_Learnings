d791c4f7be1e25bee2ab904cc96f966d83c578fc
AkshayaBaalajiS <baalaji12akshay@gmail.com>
2026-02-13 08:35:24 +0530

    Thread without join / detach will have the std::terminate called

---
+++ Thread/thread/Thread/threadWithoutJoinDetach.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <functional>
+
+using namespace std;
+
+int func(int & ref )
+{
+	int result =0;
+	cout<<"int func(int & ref )\n";
+	for(int i=0;i<10;i++)
+	{
+		auto startTime =  std::chrono::high_resolution_clock::now();
+		std::this_thread::sleep_for(std::chrono::milliseconds(1234));
+		ref+=12;
+		auto endTime =  std::chrono::high_resolution_clock::now();
+
+		auto dura = std::chrono::duration_cast<std::chrono::milliseconds>(endTime- startTime);
+		cout<<"Duration taken for iteration :" << i+1 << " = " << dura.count() <<endl;
+	}
+	result = ref;
+	return result;
+}
+
+int main()
+{
+	cout<<"This is the program to understand the thread how to get the return fro thread \n";
+	int number = 12 ;
+	thread t1(func, std::ref(number));
+
+}
