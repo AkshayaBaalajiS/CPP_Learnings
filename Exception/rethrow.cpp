2bb414209948e1bf7d0cc90168c80496eb60076d
AkshayaBaalajiS <baalaji12akshay@gmail.com>
2026-02-13 08:31:56 +0530

    rethrow is a throw on catch stmt is rethrow it will be caught the following catch

---
+++ Exception/rethrow.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+void function1()
+{
+	throw(234);
+	// throw std::runtime_error("fail");
+}
+
+int main()
+{
+	cout<<"Exception Propogation in cpp\n";
+	try
+	{
+		try
+		{
+			function1();
+		}
+		catch(...)
+		{
+			throw 0;
+		}
+
+	}
+	// oder of the catch should be proper 
+	catch(const int & e)
+	{
+		cout<<"Exception caught here :" <<e <<endl;
+	}
+	catch(const std::exception & e)
+	{
+		cout<<e.what() <<endl;
+	}
+	catch(...)
+	{
+		cout<<"Exception caught here " <<endl;
+	}
+	
+}
