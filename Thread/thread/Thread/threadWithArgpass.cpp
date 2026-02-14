bfd3541b79f9642b57dd234ae2fad0cf693e0d20
AkshayaBaalajiS <baalaji12akshay@gmail.com>
2026-02-14 19:36:02 +0530

    Threading in cpp | where the arguments are the std::promise we will get the vlaue in future and the std::Ref to be passed for the reference and some type passed || and the thread function are not able to return

---
+++ Thread/thread/Thread/threadWithArgpass.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+using namespace std;
+
+void function1(int  arg )
+{
+	cout<<"void function1(int arg )\n";
+	for(int i=0;i<10;i++)
+	{
+		auto startTime =  std::chrono::high_resolution_clock::now();
+		std::this_thread::sleep_for(std::chrono::milliseconds(1234));
+		arg+=12;
+		auto endTime =  std::chrono::high_resolution_clock::now();
+
+		auto dura = std::chrono::duration_cast<std::chrono::milliseconds>(endTime- startTime);
+		cout<<"Duration taken for iteration :" << i+1 << " = " << dura.count() <<endl;
+	}
+}
+
+void func(int & reff)
+{
+	cout<<"before reff = " <<  reff <<endl;
+	reff+=12;
+	cout<<"before reff = " <<  reff <<endl;
+	
+}
+
+int main()
+{
+	cout<<"Thread with arg passed as arg \n";
+	int number = 0;
+	func(number);
+	cout<<"number before thread = " << number <<endl;
+	thread t(function1, number );
+	t.join();
+	cout<<"number after thread = " << number <<endl;
+}
