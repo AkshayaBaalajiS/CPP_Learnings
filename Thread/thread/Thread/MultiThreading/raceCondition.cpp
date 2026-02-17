29f48f56d91f0e65ad96f883bf523fc0383d9bde
AkshayaBaalajiS <baalaji12akshay@gmail.com>
2026-02-17 10:11:46 +0530

    Race condition happens ont he code ||| What is race condition : When multiple thread try to access same global / shared variable there will be the load and store issue that is race condition that result in undefined behaviour

---
+++ Thread/thread/Thread/MultiThreading/raceCondition.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+using namespace std;
+
+struct Structure
+{
+	int a;
+	int b;
+	char c;
+	Structure() : a(12), b(12), c('a')
+	{
+
+	}
+
+	void operator+=(int a )
+	{
+		this->a+=a;
+		this->b+=b;
+		this->c+=c;
+	}
+};
+Structure * str = new Structure();
+
+mutex m1, m2;
+
+void function(int number, char b)
+{
+	cout<<"void function(int number, char b)\n";
+	for(int i=0;i<10;i++)
+	{
+		auto startTime = std::chrono::high_resolution_clock::now();
+		std::this_thread::sleep_for(std::chrono::milliseconds(1919));
+		auto endTime = std::chrono::high_resolution_clock::now();
+		{
+			lock_guard<mutex> lockk(m1);
+			str+=12;
+		}
+		auto dura = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
+		cout<<"Total Time on function "<<  i << " iteration ="  <<  dura.count() <<endl;
+	}
+}
+
+void function1(Structure * we)
+{
+	cout<<"void function1(Structure * we)\n";
+	for(int i=0;i<10;i++)
+	{
+		auto startTime = std::chrono::high_resolution_clock::now();
+		std::this_thread::sleep_for(std::chrono::milliseconds(999));
+		auto endTime = std::chrono::high_resolution_clock::now();
+		{
+			lock_guard<mutex> lockk(m2);
+			str+=12;
+		}
+		auto dura = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
+		cout<<"Total Time on function1 "<<  i << " iteration ="  <<  dura.count() <<endl;
+	}
+}
+
+
+int main()
+{
+	cout<<"Race Condition in cpp using thread\n";
+
+	thread t1(function, 12, 'a');
+	Structure * pt = new Structure();
+	thread t2(function1, pt);
+	t1.join();
+	t2.detach();
+
+
+}
