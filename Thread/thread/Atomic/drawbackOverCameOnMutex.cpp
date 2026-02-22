60867d22ce40df9fc2a9914468d0c6671ce56012
AkshayaBaalajiS <baalaji12akshay@gmail.com>
2026-02-22 21:21:37 +0530

    This code demonstrate the drawback on the atomic and how it overcame with mutex

---
+++ Thread/thread/Atomic/drawbackOverCameOnMutex.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+using namespace std;
+
+#ifdef ATOMIC 
+atomic<int> a ;
+atomic<int> b;
+#else
+int a;
+int b;
+#endif
+	
+mutex m1;
+
+void func1()
+{
+	for(int i=0;i<1000;i++)
+	{
+		{
+			std::lock_guard<std::mutex> l1(m1);
+			a++; // atomic on ++ // read load and write will be atomic 
+			// cout<<"[func1] a =" << a <<endl; // cout in cpp internally uses mutex to lock the thread for printing  
+			b++; // atomic on ++ // read load and write will be atomic 
+			// cout<<"[func1] b =" << b <<endl; // cout in cpp internally uses mutex to lock the thread for printing  
+		}
+	}
+}
+void func2()
+{
+	for(int i=0;i<1000;i++)
+	{
+		{
+			std::lock_guard<std::mutex> l1(m1);
+			a++; // atomic on ++ // read load and write will be atomic 
+			// cout<<"[func2] a =" << a <<endl; // cout in cpp internally uses mutex to lock the thread for printing  
+			b++; // atomic on ++ // read load and write will be atomic 
+			// cout<<"[func2] b =" << b <<endl; // cout in cpp internally uses mutex to lock the thread for printing  
+		}
+	}
+}
+
+void observer()
+{
+	for (int i = 0; i < 100000; i++) {
+		#ifdef ATOMIC
+        {
+        	std::lock_guard<mutex> l1(m1);
+        	int x = a.load();
+	        int y = b.load();
+	        if (x != y) {
+	            cout << "MISMATCH a=" << x << " b=" << y << endl;
+	            break;
+	        }
+	    }
+        #else
+        {
+        	std::lock_guard<mutex> l1(m1);
+        	int x= a;
+	        int y= b;
+	        if (x != y) {
+	            cout << "MISMATCH a=" << x << " b=" << y << endl;
+	            break;
+	        }
+	    }
+        #endif
+        
+    }
+}
+
+int main()
+{
+	cout<<"Simple code to show tyhe atomic \n";
+	thread t3(observer);
+	thread t1(func1);
+	thread t2(func2);
+	t2.join();
+	t1.join();
+	t3.join();
+	cout<<"A = " << a <<endl;
+	cout<<"B = " << b <<endl;
+
+}
+/*
+Simple code to show tyhe atomic 
+MISMATCH a=1284 b=1283
+A = 2000
+B = 2000
+
+this si the output where we can see the atomic does not guarantewe concurrency 
+*/
+/*
+Atomics guarantee correct final values.
+They do NOT guarantee that multiple atomics change together as one unit.
+*/
+
+/*
+abt cout in thread 
+Key reason #1 (MOST IMPORTANT): std::cout is synchronized
+
+This line is the hidden villain:
+
+cout << "[func1] a =" << a << endl;
+
+What actually happens internally
+
+std::cout is thread-safe
+
+It uses an internal mutex
+
+While one thread is printing, the other thread cannot print
+
+So your code effectively becomes:
+
+lock(cout_mutex)
+print a
+unlock(cout_mutex)
+
+*/
+
+/* 
+abt he atomics 
+The statement:
+
+“Atomics guarantee correct final values, but not grouped updates”
+*/
