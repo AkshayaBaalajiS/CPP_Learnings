bb535f4bf7672bc5fc875b42a4be4aa4e06fa910
AkshayaBaalajiS <baalaji12akshay@gmail.com>
2026-03-02 10:20:21 +0530

    promise and future in cpp in thread it will be helpful

---
+++ Thread/thread/ReturnFromThread/promise_and_future.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <future>
+#include <thread>
+#include <functional>
+
+using namespace std;
+
+void function1(std::promise<int> p)
+{
+	int value = 234;
+	std::this_thread::sleep_for(std::chrono::milliseconds(12345));
+	p.set_value(value);
+}
+
+int main()
+{
+	cout<<"promise and future in cpp for taking value from thread function1 \n";
+	std::promise<int> p ; //std::promise is non-copyable and must be transferred (moved) into the thread.
+	std::future<int> f = p.get_future();
+	// promise is not copyable so moved to thread 
+	// thread t(function1, p);
+	thread t(function1, std::move(p));
+	int result = f.get(); 
+	cout<<"result from threadf before join = " << result <<endl;
+	t.join();
+	result = f.get(); // future::get() BLOCKS until the promise sets the value.
+	cout<<"result from threadf after join = " << result <<endl;
+}
