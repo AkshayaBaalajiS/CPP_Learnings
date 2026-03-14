2c8e457352b751a9cdb089b333b5f26e7e918574
Akshaya Baalaji S <baalaji12akshay@gmail.com>
2026-03-14 08:27:36 +0000

    new opeartor in cpp

---
+++ MemoryAllocationDeallocation/new/new.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Sample
+{
+	public:
+		Sample()
+		{
+			cout<<"@ Sample class constructor\n";
+		}
+
+		~Sample()
+		{
+			cout<<"@ Sample class destructor\n";
+		}
+};
+
+int main()
+{
+	cout<<"This is the code to understand the new working in cpp \n";
+	Sample * ptr = new Sample();
+
+	delete ptr;
+	// delete(ptr);
+}
