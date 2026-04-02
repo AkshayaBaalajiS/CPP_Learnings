de0f146c90315c24da96d6713edde20d99df2264
Akshaya Baalaji S <baalaji12akshay@gmail.com>
2026-04-02 17:35:31 +0000

    Dynamic library manual understanding

---
+++ Library_Static_Dynamic/Dynamic_Library/prac_1/divide.cpp
@@ -0,0 +1,5 @@
+#include "divide.h"
+void divide(int a, int b)
+{
+    std::cout << "A / B = " << a / b << std::endl;
+}
---
+++ Library_Static_Dynamic/Dynamic_Library/prac_1/divide.h
@@ -0,0 +1,8 @@
+#ifndef DIVIDE_H
+#define DIVIDE_H
+#include<iostream>
+
+// Definition of the divide function
+void divide(int a, int b);
+
+#endif // DIVIDE_H
